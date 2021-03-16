5d92164fe15ffc12261bc72287604da2f4a8b9a0
Mona Honkanen <43002985+Monahonkanen@users.noreply.github.com>
2021-03-16 20:49:52 +0200

    Update program.py
    Changed comments

--- 9Solutions_Task/program.py
+++ 9Solutions_Task/program.py
@@ -1,9 +1,9 @@
 """
-This is a simple program that reads user input from localhols web page and compares it to config files list.
-If it finds a mathc, programm will send an email.
-There is tempalates file witch has page.html -file.
+This is a simple program that reads user input from localhost web page and compares it to config files list.
+If it finds a match, program will send an email.
+There is templates file witch has page.html -file.
 There is also config.ini file.
-For email sendig is used yagmail library
+For email sending is used yagmail library
 """
 
 from flask import Flask, render_template,request
